743b5cec0e5beb84029beda3cb63ed5eb7e15f6e
pratap47 <agrawal.akash407@gmail.com>
2019-05-14 20:09:27 +0530

    student dashboard done

--- app/src/main/java/test/com/disha/StudentDashboard.java
+++ app/src/main/java/test/com/disha/StudentDashboard.java
@@ -1,41 +1,175 @@
 package test.com.disha;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
 public class StudentDashboard extends AppCompatActivity {
 
     Spinner spinner;
+    StudentDashboardAdapter mAdapter;
+    ArrayList<String> mQuestions ;
+    // DatabaseReference mDatabase;
+    RecyclerView recyclerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_dashboard);
+
+        // mDatabase = FirebaseDatabase.getInstance().getReference().child("Stream");
+        mQuestions = new ArrayList<>();
+
+
         spinner = (Spinner)findViewById(R.id.spinner);
-        String[] mField = {"Engineering-CSE","Engineering-ETC","Engineering-Mech","MBA","Medical"};
+        recyclerView = (RecyclerView)findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+
+
+        String[] mField = {"Engineering-IT","Engineering-ETC","Engineering-Mech","MBA","Medical"};
         ArrayAdapter mSA = new ArrayAdapter(this,android.R.layout.simple_spinner_item, mField);
         spinner.setAdapter(mSA);
+
+
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int pos, long l) {
 
+                if(pos==0){
+                    mQuestions.clear();
+                    DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference().child("Stream").child("EngineeringIT");
+                    mDatabase.orderByValue().addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                            Iterable<DataSnapshot> mChildren = dataSnapshot.getChildren();
+                            for(DataSnapshot mChild:mChildren){
+                                String mQues = mChild.child("question").getValue().toString();
+                                mQuestions.add(mQues);
+                                String debug = mQues;
+                                int d = mQuestions.size();
+                                mAdapter.notifyItemInserted(mQuestions.size()-1);
+                            }
+                        }
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError databaseError) {
+                        }
+                    });
+                    mAdapter = new StudentDashboardAdapter(getApplicationContext(),mQuestions);
+                    recyclerView.setAdapter(mAdapter);
+                }
                 if(pos==1){
+                    mQuestions.clear();
+                    DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference().child("Stream").child("EngineeringETC");
+                    mDatabase.orderByValue().addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
 
+                            Iterable<DataSnapshot> mChildren = dataSnapshot.getChildren();
+                            for(DataSnapshot mChild:mChildren){
+                                String mQues = mChild.child("question").getValue().toString();
+                                mQuestions.add(mQues);
+                                mAdapter.notifyItemInserted(mQuestions.size()-1);
+                            }
+                        }
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                        }
+                    });
+                    mAdapter = new StudentDashboardAdapter(getApplicationContext(),mQuestions);
+                    recyclerView.setAdapter(mAdapter);
                 }
-                else if(pos==2){
+                if(pos==2){
+                    mQuestions.clear();
+                    DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference().child("Stream").child("EngineeringMECH");
+                    mDatabase.orderByValue().addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
 
+                            Iterable<DataSnapshot> mChildren = dataSnapshot.getChildren();
+                            for(DataSnapshot mChild:mChildren){
+                                String mQues = mChild.child("question").getValue().toString();
+                                mQuestions.add(mQues);
+                                mAdapter.notifyItemInserted(mQuestions.size()-1);
+                            }
+
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                        }
+                    });
+                    mAdapter = new StudentDashboardAdapter(getApplicationContext(),mQuestions);
+                    recyclerView.setAdapter(mAdapter);
                 }
-                else if(pos==3){
+                if(pos==3){
+                    mQuestions.clear();
+                    DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference().child("Stream").child("MBA");
+                    mDatabase.orderByValue().addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
 
+                            Iterable<DataSnapshot> mChildren = dataSnapshot.getChildren();
+                            for(DataSnapshot mChild:mChildren){
+                                String mQues = mChild.child("question").getValue().toString();
+                                mQuestions.add(mQues);
+                                String debug = mQues;
+                                int d = mQuestions.size();
+                                int l =d;
+                                mAdapter.notifyItemInserted(mQuestions.size()-1);
+                            }
+
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                        }
+                    });
+                    mAdapter = new StudentDashboardAdapter(getApplicationContext(),mQuestions);
+                    recyclerView.setAdapter(mAdapter);
                 }
-                else if(pos==4){
+                if(pos ==4){
+                    mQuestions.clear();
+                    DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference().child("Stream").child("Medical");
+                    mDatabase.orderByValue().addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
 
-                }
-                else if(pos ==5){
+                            Iterable<DataSnapshot> mChildren = dataSnapshot.getChildren();
+                            for(DataSnapshot mChild:mChildren){
+                                String mQues = mChild.child("question").getValue().toString();
+                                mQuestions.add(mQues);
+                                mAdapter.notifyItemInserted(mQuestions.size()-1);
+                            }
 
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                        }
+                    });
+                    mAdapter = new StudentDashboardAdapter(getApplicationContext(),mQuestions);
+                    recyclerView.setAdapter(mAdapter);
                 }
 
 
@@ -43,8 +177,30 @@ public class StudentDashboard extends AppCompatActivity {
 
             @Override
             public void onNothingSelected(AdapterView<?> adapterView) {
+//                DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference().child("Stream").child("EngineeringIT");
+//                mDatabase.orderByValue().addListenerForSingleValueEvent(new ValueEventListener() {
+//                    @Override
+//                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+//
+//                        Iterable<DataSnapshot> mChildren = dataSnapshot.getChildren();
+//                        for(DataSnapshot mChild:mChildren){
+//                            String mQues = mChild.child("question").getValue().toString();
+//                            mQuestions.add(mQues);
+//
+//                        }
+//
+//                    }
+//
+//                    @Override
+//                    public void onCancelled(@NonNull DatabaseError databaseError) {
+//
+//                    }
+//                });
+
 
             }
         });
+
+
     }
 }
---
+++ app/src/main/java/test/com/disha/StudentDashboardAdapter.java
@@ -0,0 +1,53 @@
+package test.com.disha;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class StudentDashboardAdapter extends RecyclerView.Adapter<StudentDashboardAdapter.ViewHolder> {
+
+    private Context mContext;
+    private ArrayList<String> mQuestions = new ArrayList<>();
+
+    public StudentDashboardAdapter(Context mContext, ArrayList<String> mQuestions) {
+        this.mContext = mContext;
+        this.mQuestions = mQuestions;
+    }
+
+    @NonNull
+    @Override
+    public StudentDashboardAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.listitem_student_dashboard,viewGroup,false);
+        ViewHolder holder = new ViewHolder(view);
+        return holder;
+
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull StudentDashboardAdapter.ViewHolder viewHolder, int i) {
+
+        String mQue = mQuestions.get(i);
+        viewHolder.txtQuestion.setText(mQue);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mQuestions.size();
+    }
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView txtQuestion;
+        RelativeLayout adapQuestion;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            txtQuestion = itemView.findViewById(R.id.txtQuestion);
+            adapQuestion = itemView.findViewById(R.id.adapQuestion);
+        }
+    }
+}
